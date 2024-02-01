8210404f724978449829333b547ea9d09e771bf4
dev-chpark <xpmxf5@gmail.com>
2024-02-01 16:52:35 +0900

    ⭐ feat: 임시 SecurityConfig 작성

--- src/main/java/com/uahannam/java/config/SecurityConfig.java
+++ src/main/java/com/uahannam/java/config/SecurityConfig.java
@@ -1,11 +1,24 @@
 package com.uahannam.java.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authz -> authz.anyRequest().permitAll());
 
+        return http.build();
+    }
 }
