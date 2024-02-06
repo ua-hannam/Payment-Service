72f13d54beb169cbdf0c43361bbc0c7bc87c5b1c
dev-chpark <xpmxf5@gmail.com>
2024-02-06 17:56:31 +0900

    ⚙️ chore: import문 삭제

--- src/main/java/com/uahannam/java/controller/PaymentController.java
+++ src/main/java/com/uahannam/java/controller/PaymentController.java
@@ -1,10 +1,7 @@
 package com.uahannam.java.controller;
 
-import com.uahannam.java.service.PaymentService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
