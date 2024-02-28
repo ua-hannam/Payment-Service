dca18d20e7d98ec4d1651f9284dd7c65df650505
dev-chpark <xpmxf5@gmail.com>
2024-02-28 17:15:17 +0900

    ♻️ refactor: 불필요한 PostDto 제거

--- src/main/java/com/uahannam/java/dto/request/PostDto.java
+++ src/main/java/com/uahannam/java/dto/request/PostDto.java
@@ -1,10 +1,14 @@
 package com.uahannam.java.dto.request;
 
 import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 
 @Getter
-public class PostDto extends BaseDto {
+public class PostDto {
+
+    @NotNull(message = "사용자 id 는 필수입니다.")
+    private String userId;
 
     @Min(value = 1, message = "충전 금액은 1 이상이어야 합니다.")
     private Integer amount;
